fc9fd44166f5611617ce388d02b17b551ad041ae
rosemary.kimani <rosemary.kimani@student.moringaschool.com>
2022-08-17 18:03:16 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+const multiply = num1 * num2;
+
+const random = Math.ceil(Math.random() * 5
+ ) + 1;
+
+ let num3 = 19;
+let num4 = 5;
+const mod = num3 % num4;
+
+const max = Math.max(3, 7, 9, 11, 20);
